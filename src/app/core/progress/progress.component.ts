c277614ac906dc32e0099c42a2c6e252d8f9ad3c
Joy <joycok@gmail.com>
2019-10-29 17:53:55 +0530

    save

--- src/app/core/progress/progress.component.ts
+++ src/app/core/progress/progress.component.ts
@@ -7,24 +7,16 @@ import { interval } from 'rxjs';
 })
 
 export class ProgressComponent implements OnInit {
-    cvalue = 0;
-    _interval$: any;
+    
     constructor(
 
     ) { }
 
     ngOnInit() {
-        this._interval$ = interval(250).subscribe(
-            e => {
-                this.cvalue++;
-                if (this.cvalue == 100)
-                    this.cvalue = 0;
-            }
-        )
+
     }
 
     ngOnDestroy() {
-        this._interval$.unsubscribe();
     }
 
 
