7b253e35d96d4d560fc481ef9b31854525b722c9
Joy <joycok@gmail.com>
2019-10-31 15:02:50 +0530

    saev

--- src/app/core/login/auth.effects.ts
+++ src/app/core/login/auth.effects.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { login_action, logout_action, login_success_action, login_fail_action } from './auth.store';
-import { LoginService } from 'src/app/services/login.service';
-
-import { of } from 'rxjs';
-import { tap, switchMap, map, catchError } from 'rxjs/operators';
-import { Iuser } from '../../models/iuser';
 import { Router } from '@angular/router';
 
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { login_action, logout_action, login_success_action, login_fail_action } from './auth.store';
+import { of } from 'rxjs';
+import { tap, switchMap, map, catchError } from 'rxjs/operators';
+
+import { LoginService } from '../../services/login.service';
+import { Iuser } from '../../models/iuser';
 
 @Injectable()
 export class AuthEffects {
